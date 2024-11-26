c187491213ab7405b3c1d6667239d2450e10b738
Ubuntu <ubuntu@ip-172-31-46-48.eu-west-2.compute.internal>
2024-11-26 17:44:48 +0000

    Cannot compile

---
+++ rs_lib/pocrust_lib/pocrust.c
@@ -0,0 +1,11 @@
+/*
+const char* pocc_f()
+{
+    return "Hello from C\n";
+}
+*/
+
+
+float pocc_f() {
+    return 1.77;
+}
--- rs_lib/src/lib.rs
+++ rs_lib/src/lib.rs
@@ -1,11 +1,27 @@
 use wasm_bindgen::prelude::*;
-use std::fs;
+// use std::fs;
 
+/*
 #[wasm_bindgen]
 pub fn poc(file_path: String) -> String{
     let contents = fs::read_to_string(file_path).expect("The file should be read");
     return contents;
 }
+*/
+
+#[link(name = "pocrust", kind = "static")]
+extern "C" {
+    // fn pocc_f() -> *const c_char;
+    pub fn pocc_f() -> f32;
+}
+
+/*
+#[wasm_bindgen]
+pub fn rust_string() -> String {
+  let cstr = unsafe { CStr::from_ptr(pocc_f()) };
+  String::from_utf8_lossy(cstr.to_bytes()).to_string()
+}
+*/
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
